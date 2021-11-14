110452d7a39c4ff5bfe55029d4a5736da3c63fea
mattcmerritt <mattcmerritt@gmail.com>
2021-11-14 10:10:14 -0500

    intersection class contains references to adjacent intersections

--- src/Intersection.java
+++ src/Intersection.java
@@ -1,3 +1,22 @@
+import java.util.ArrayList;
+
 public class Intersection {
     
+    private ArrayList<Intersection> connectedIntersections;
+
+    public Intersection()
+    {
+        connectedIntersections = new ArrayList<Intersection>();
+    }
+
+    public void addIntersection(Intersection i)
+    {
+        connectedIntersections.add(i);
+    }
+
+    public Intersection[] getConnectedIntersections()
+    {
+        Intersection[] intersections = (Intersection[]) connectedIntersections.toArray();
+        return intersections;
+    }
 }
